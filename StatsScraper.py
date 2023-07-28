a51b450295bfc37f546b5d1cc1dcb87555ee845a
chrisvalk <108788778+chrisvalk@users.noreply.github.com>
2023-07-28 01:29:11 -0400

    Update StatsScraper.py
    Logging Configuration: In the new code, the logging configuration has been modified to set up a custom logger named __name__. The log messages are written to both a log file named "Statsscraper.logs" and also shown in the console (StreamHandler).
    
    Code Organization: The new code includes comments and better organization with appropriate indentation and spacing, making it more readable and maintainable.
    
    Function Renaming: The function isFullData has been renamed to is_Full_Data to follow PEP8 naming conventions (using snake_case for function names).
    
    Helper Function Improvements: The helper functions getDataLink and getDayStr have been modified to use string interpolation instead of string concatenation for constructing URLs.
    
    Constant for Day Count: A constant DAY_COUNT has been introduced to make it clear that the script fetches data for the last 7 days.
    
    Handling Incomplete Data: The new code includes a process_item_data function to handle incomplete data for each item fetched. If required keys are missing or any error occurs during data processing, the function logs a warning and skips that item's data.
    
    Dictionary for Item Subtypes: A dictionary name_to_subtype has been introduced to map item subtypes to their names. This is used in the process_item_data function to handle item data with varying lengths depending on the subtype.
    
    Usage of try-except Blocks: In several places, the new code includes try-except blocks to catch exceptions and log appropriate warning messages while processing data.
    
    Logging of Script Completion: The new code includes a log message indicating the completion of script execution.
    
    Improved CSV File Saving: The new code attempts to save the data to the CSV file using the to_csv function, and if any error occurs, it logs an error message.
    
    Removal of Backup CSV File: The new code removes the backup CSV file if it exists, and also sets the configuration status to indicate that the script is no longer running.

--- StatsScraper.py
+++ StatsScraper.py
@@ -1,3 +1,4 @@
+# Import required libraries
 import json, requests
 from datetime import datetime, timedelta
 from tqdm import tqdm
@@ -8,106 +9,130 @@ import time
 import config
 import logging
 
+# Set up logging configuration
 current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Define the log file path
 log_file_path = os.path.join(current_directory, "Statsscraper.logs")
 
-# Configure logging settings
 logging.basicConfig(level=logging.DEBUG, format='{levelname:7} {message}', style='{',
                     handlers=[
                         logging.FileHandler(log_file_path),
                         logging.StreamHandler()
                     ])
 
-# Set up the logger
 logger = logging.getLogger(__name__)
 
+# Fetch item data from the Warframe Market API
 allItemsLink = "https://api.warframe.market/v1/items"
 r = requests.get(allItemsLink)
 itemList = r.json()["payload"]["items"]
 itemNameList = [x["url_name"] for x in itemList if "relic" not in x["url_name"]]
-urlLookup = {x["item_name"] : x["url_name"] for x in itemList}
+urlLookup = {x["item_name"]: x["url_name"] for x in itemList}
 
+# Set file names for the main and backup CSV files
 csvFileName = "allItemData.csv"
+backupCsvFileName = "allItemDataBackup.csv"
 
+# Create a backup of the main CSV file if it already exists
 try:
-    os.rename(csvFileName, "allItemDataBackup.csv")
+    os.rename(csvFileName, backupCsvFileName)
+    logger.info("Created a backup of the main CSV file.")
 except FileNotFoundError:
     pass
 except FileExistsError:
     config.setConfigStatus("runningStatisticsScraper", False)
-    raise Exception("Remove the backup or the main csv file, one shouldn't be there for this to run.")
+    raise Exception("Remove the backup or the main CSV file. Only one should be present for this script to run.")
 
-
+# Helper function to check if data is complete
 def is_Full_Data(data):
     if len(data) == 0:
         return False
     if "mod_rank" in data[0].keys() and len(data) == 6:
         return True
-    if "mod_rank" not in data[0].keys() and len(data) == 3:
+    if "mod_rank" not in data[0].keys() and len(data) in [3, 4]:
         return True
     return False
 
+# Helper function to construct data link for a given day
 def getDataLink(dayStr):
     if config.platform != "pc":
         return f"https://relics.run/history/{config.platform}/price_history_{dayStr}.json"
     else:
         return f"https://relics.run/history/price_history_{dayStr}.json"
 
+# Helper function to get the date string for a given number of days back
 def getDayStr(daysBack):
     day = datetime.now() - timedelta(daysBack)
     dayStr = datetime.strftime(day, '%Y-%m-%d')
     return dayStr
 
-# Define the number of days to retrieve data for
+# Define the number of days for which data is to be fetched
 DAY_COUNT = 7
 last_seven_days = [getDayStr(x) for x in range(1, DAY_COUNT + 1)]
+
+# Initialize an empty DataFrame to store the data
 df = pd.DataFrame()
 found_data = 0
 
-# Loop through the last seven days and scrape data from each day
+# Dictionary to map item subtypes to their names
+name_to_subtype = {
+    "radiant": "radiant",
+    "intact": "intact",
+    "flawless": "flawless"
+}
+
+# Function to process item data and create DataFrames for each item
+def process_item_data(data, item_name):
+    try:
+        item_df = pd.DataFrame.from_dict(data)
+        required_keys = {"datetime", "volume", "min_price", "max_price"}
+        missing_keys = required_keys.difference(item_df.columns)
+        if missing_keys:
+            logger.warning(f"Skipping incomplete data for {item_name} on {day_str} due to missing keys: {missing_keys}.")
+            return None
+        
+        columns_to_drop = ["open_price", "closed_price", "donch_top", "donch_bot"]
+        columns_to_drop = [col for col in columns_to_drop if col in item_df.columns]
+        itemDF = item_df.drop(columns_to_drop, axis=1)
+
+        itemDF = itemDF.fillna({"order_type": "closed"})
+        itemDF["name"] = urlLookup[item_name]
+        itemDF["range"] = itemDF["max_price"] - itemDF["min_price"]
+        if "mod_rank" not in itemDF.columns:
+            itemDF["mod_rank"] = np.nan
+        else:
+            itemDF = itemDF[itemDF["mod_rank"] != 0]
+
+        itemDF = itemDF[["name", "datetime", "order_type", "volume", "min_price", "max_price", "range", "median", "avg_price", "mod_rank"]]
+        return itemDF
+    except Exception as e:
+        logger.warning(f"Error processing data for {item_name} on {day_str}: {e}")
+        return None
+
+# Loop through the last seven days to fetch and process item data
 for day_str in tqdm(last_seven_days):
     link = getDataLink(day_str)
     response = requests.get(link)
     if str(response.status_code)[0] != "2" or found_data >= 7:
-        # If there was an error or we already found data for 7 days, continue to the next day
         continue
     found_data += 1
     for name, data in response.json().items():
         if is_Full_Data(data):
-            # Create a DataFrame from the fetched data and process it
-            item_df = pd.DataFrame.from_dict(data)
-            try:
-                itemDF = item_df.drop(["open_price", "closed_price", "donch_top", "donch_bot"], axis=1)
-                itemDF = item_df.fillna({"order_type" : "closed"})
-                itemDF["name"] = urlLookup[name]
-                itemDF["range"] = itemDF["max_price"] - itemDF["min_price"]
-                if "mod_rank" not in itemDF.columns:
-                    itemDF["mod_rank"] = np.nan
-                else:
-                    itemDF = itemDF[itemDF["mod_rank"] != 0]
-                #display(itemDF)
-                
-                itemDF = itemDF[["name", "datetime", "order_type", "volume", "min_price", "max_price","range", "median", "avg_price", "mod_rank"]]
-                
+            itemDF = process_item_data(data, name)
+            if itemDF is not None:
                 df = pd.concat([df, itemDF])
-            except KeyError:
-                logger.warning(f"Skipping incomplete data for {name} on {day_str} due to missing keys.")
-    
 
+# Filter out items with insufficient data and select popular items
 countDF = df.groupby("name").count().reset_index()
 popularItems = countDF[countDF["datetime"] == 21]["name"]
 df = df[df["name"].isin(popularItems)]
 df = df.sort_values(by="name")
 itemListDF = pd.DataFrame.from_dict(itemList)
-#itemListDF
-#df = df.drop("Unnamed: 0", axis=1)
 df["item_id"] = df.apply(lambda row : itemListDF[itemListDF["url_name"] == row["name"]].reset_index().loc[0, "id"], axis=1)
 df["order_type"] = df.get("order_type").str.lower()
-df.to_csv("allItemDataBackup.csv", index=False)
 
-# Save the scraped data to a CSV file
+# Save the data to CSV file
+df.to_csv("allItemData.csv", index=False)
+
 try:
     df.to_csv(csvFileName, index=False)
     logger.info("Data saved to CSV file.")
@@ -115,7 +140,10 @@ except Exception as e:
     logger.error(f"Failed to save data to CSV file: {e}")
 
 # Remove the backup CSV file and mark the script as not running in the configuration
-os.remove("allItemDataBackup.csv")
+if os.path.exists(backupCsvFileName):
+    os.remove(backupCsvFileName)
+    logger.info("Backup CSV file removed.")
 config.setConfigStatus("runningStatisticsScraper", False)
+
 # End of the script
 logger.info("Script execution completed.")
