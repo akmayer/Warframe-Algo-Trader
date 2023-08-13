6414e5d77b2499f044f25bb6248382c1d923d3d1
akmayer <akmayer@gmail.com>
2023-08-13 10:12:16 -0700

    customLogger.py verifies that logs dir exists before writing.

--- customLogger.py
+++ customLogger.py
@@ -2,15 +2,25 @@ import os
 from datetime import datetime
 logDir = "logs"
 
+def verifyLogDir():
+    if os.path.exists(os.path.join(logDir, ".gitignore")):
+        return
+    if os.path.exists(logDir):
+        return
+    os.mkdir(logDir)
+    with open(os.path.join(logDir, ".gitignore"), "w") as gitignore:
+        gitignore.write("# Ignore everything in this directory\n*\n# Except this file\n!.gitignore")
+
 def writeTo(logFileName, line):
+    verifyLogDir()
     with open(os.path.join(logDir, logFileName), "a") as logFile:
         logFile.write(f"{datetime.today()}\t{line}\n")
 
 def clearFile(logFileName):
+    verifyLogDir()
     with open(os.path.join(logDir, logFileName), "w") as logFile:
         logFile.write("")
 
 
 if __name__ == "__main__":
-    writeTo("apiCalls.log", "Some logging")
-    #clearFile("apiCalls.log")
+    verifyLogDir()
