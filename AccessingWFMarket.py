4722bf75d3c8d4890a5f513e392f0d429e784309
akmayer <akmayer@gmail.com>
2023-08-14 22:27:59 -0700

    Increased Version Number

--- AccessingWFMarket.py
+++ AccessingWFMarket.py
@@ -17,7 +17,7 @@ class WarframeApi:
             "platform": config.platform,
             "language": "en",
             "Authorization": self.jwt_token,
-            'User-Agent': 'Warframe Algo Trader/1.2.6',
+            'User-Agent': 'Warframe Algo Trader/1.2.7',
         }
         self.lastRequestTime = 0
         self.timeBetweenRequests = 3
