0bc3e4dfa5784a9494c12f101435a9f86ad7b93a
A Mayer <akmayer@gmail.com>
2023-08-29 20:13:53 -0700

    Close Settings Setup

--- my-app/app/settings.tsx
+++ my-app/app/settings.tsx
@@ -168,7 +168,7 @@ export default function Settings({
           </div>
           <div className="half-wide left">
             <button
-            onClick={writeSettings}>
+            onClick={() => {writeSettings(); onShow();}}>
             Save
             </button>
           </div>
